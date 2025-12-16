4663d0869fccac393d5c1a2b8afb04f9af688c75
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-15 21:23:18 -0500

    eje2-Paso 4: Modificamos la logica para hacer una funcion mas completa que permita modificar las notas y el promedio

--- eje2.py
+++ eje2.py
@@ -10,24 +10,36 @@ def calcular_promedio (notas):
     peor_nota = min(notas)
     mejor_nota = max(notas)
 
-    # calculamos una nueva suma
+    # buscamos el indice de la peor nota
 
-    nueva_suma = suma - peor_nota + mejor_nota
+    indice_peor = notas.index(peor_nota)
 
-    # Promedio
-    promedio = nueva_suma / len(notas) 
+    # remplazamos la peor nota por la mejor
+    notas[indice_peor] = mejor_nota
+
+    # calculamos el nuevo promedio
+    promedio = sum(notas) / len(notas)
 
     return promedio
 
 # Probamos la función con una lista de notas
+
 mis_notas = [12, 15, 17, 14]
+print(f"Notas originales: {mis_notas}")
+
 resultado = calcular_promedio(mis_notas)
 
+print ("Notas modificadas: ", mis_notas)
 print(f"El promedio modificado es: {resultado}")
 
 #-----------------------------------------------------------
 
 # Probamos la función con otra lista de notas
 otras_notas = [10, 20, 15, 18, 14]
+print(f"\nNotas originales: {otras_notas}")
+
 resultado2 = calcular_promedio(otras_notas)
+print ("Notas modificadas: ", otras_notas)
 print(f"El promedio modificado es: {resultado2}")
+
+#-----------------------------------------------------------
