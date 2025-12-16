052e88a51a93f1c4df05c86b983a012b699814b7
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-15 20:47:33 -0500

    Paso 2: Imprimir tomando en cuenta restricciones

--- eje1.py
+++ eje1.py
@@ -1,3 +1,6 @@
 #Pedimos información al usuaio
 usuario = input("Ingrese el nombre de usuario: ")
 dominio = input("Ingrese el dominio: ")
+
+#Imprimimos el mensaje tomando en cuenta las restricciones
+print(f"{usuario}@{dominio}")
