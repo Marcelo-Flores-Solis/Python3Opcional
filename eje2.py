2d5ffa0ad0625b2ef1735c149fa4761a591d3fc5
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-15 21:04:17 -0500

    eje2- Paso 1: Vamos creando nuestra funcion y recolectando datos

---
+++ eje2.py
@@ -0,0 +1,12 @@
+# Creamos funcion
+
+def calcular_promedio (notas):
+    
+    #sumamos las notas 
+    suma = sum(notas)
+
+    #buscamos la peor y mejor nota
+
+    peor_nota = min(notas)
+    mejor_nota = max(notas)
+    
