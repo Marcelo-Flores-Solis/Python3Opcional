bff6f2c42013cc65f66209a2569bdca9e55ecaba
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-15 21:08:40 -0500

    eje2- Paso 3: Probamos la funcion con el ejemplo dado y usamos otro ejemplo para reafirmar

--- eje2.py
+++ eje2.py
@@ -12,9 +12,22 @@ def calcular_promedio (notas):
 
     # calculamos una nueva suma
 
-    nueva_suma = suma - peor_nota - mejor_nota
+    nueva_suma = suma - peor_nota + mejor_nota
 
     # Promedio
     promedio = nueva_suma / len(notas) 
 
     return promedio
+
+# Probamos la función con una lista de notas
+mis_notas = [12, 15, 17, 14]
+resultado = calcular_promedio(mis_notas)
+
+print(f"El promedio modificado es: {resultado}")
+
+#-----------------------------------------------------------
+
+# Probamos la función con otra lista de notas
+otras_notas = [10, 20, 15, 18, 14]
+resultado2 = calcular_promedio(otras_notas)
+print(f"El promedio modificado es: {resultado2}")
